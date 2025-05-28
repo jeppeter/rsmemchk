ddebed106e8ed7a8ab334eb834f290d4a169282d
jeppeter <jeppeter@gmail.com>
2025-05-28 14:18:29 +0800

    find all not deallocated memory are the static variable

--- example/extparser.rs
+++ example/extparser.rs
@@ -34,7 +34,7 @@ use rsmalloc::{StackCallAllocEx,MemoryInfo};
 
 
 #[global_allocator]
-static ALLOCATOR: StackCallAllocEx = StackCallAllocEx{memsize : 307,stacksize:16};
+static ALLOCATOR: StackCallAllocEx = StackCallAllocEx{memsize : 307,stacksize:32};
 
 
 extargs_error_class!{ExtParserError}
